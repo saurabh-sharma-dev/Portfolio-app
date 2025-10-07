f2b71ef430289f300e2be3db1aa434eb8eee78ce
Saurabh Kumar Sharma <saurabhkumarsharma840@gmail.com>
2025-10-07 19:34:15 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://portfolio-app-livid-eight.vercel.app/",
+  "https://portfolio-app-orih.vercel.app/",
 ].filter(Boolean);
 
 // ---------------------- MIDDLEWARE ----------------------
